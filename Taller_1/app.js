1c6a65ea8649d2f4b2a852b8777d374c827ef08a
DanielSanchez052 <sanchezd0528@gmail.com>
2021-08-21 21:25:58 -0500

    Se inicia el Taller 1

---
+++ Taller_1/app.js
@@ -0,0 +1,35 @@
+let coordenada_x1 = 0;
+let coordenada_x2 = 50;
+let coordenada_y1 = 0;
+let coordenada_y2 = -70;
+
+function CalcularDistancia(
+  coordenada_x1,
+  coordenada_x2,
+  coordenada_y1,
+  coordenada_y2
+) {
+  const distancia = Math.sqrt(
+    Math.pow(coordenada_x2 - coordenada_x1, 2) +
+      Math.pow(coordenada_y2 - coordenada_y1, 2)
+  );
+  return distancia;
+}
+
+console.log(
+  `La distancia del plneta oooo a Naboo es de: ${CalcularDistancia(
+    coordenada_x1,
+    coordenada_x2,
+    coordenada_y1,
+    coordenada_y2
+  )} UA `
+);
+
+console.log(
+  `La distancia del plneta aaaa a Naboo es de: ${CalcularDistancia(
+    0,
+    20,
+    0,
+    30
+  )} UA `
+);
